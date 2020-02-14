92e54ec431a84201e36d4e19b7ef5deb0f079340
Mike Tellinghuisen <mtellin@mtrbkmbp.local>
2020-02-14 16:05:35 -0600

    added bash script for Epic file restores

---
+++ Shell/Restore-File.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# REQUIREMENTS:
+#   - curl is required to interact with the API
+#   - jq is required to parse JSON responses
+
+#Set the following environment variables:
+#$RUBRIK_NODE
+#$RUBRIK_USER
+#$RUBRIK_PASS
+
+# Replace the ::: with %3A%3A%3A
+FILESET_ID='Fileset%3A%3A%3Afddfc2fa-b67a-4f1c-b920-b31c45dc8661'
+
+# Grab this from developer tools after running the restore in the UI. Copy and paste after clicking 'view source'
+BODY='{"restoreConfig":[{"path":"C:\\Users\\mike.tellinghuisen\\Documents\\Critical Password List.pdf","restorePath":"c:\\temp"}],"ignoreErrors":false}'
+
+LATEST_ID=$(curl -k -s -u "$RUBRIK_USER:$RUBRIK_PASS" -X GET "https://$RUBRIK_NODE/api/v1/fileset/$FILESET_ID" | jq -r '.snapshots[-1].id')
+
+curl -k -s -u "$RUBRIK_USER:$RUBRIK_PASS" -X POST -d $BODY \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    "https://$RUBRIK_NODE/api/internal/fileset/snapshot/$LATEST_ID/restore_files"
