cc5d51ec0f015b9f78999a0d2b79638dda824f93
Mike Tellinghuisen <mtellin@gmail.com>
2020-08-24 12:57:42 -0500

    added adding a SQL AG to an existing Rubrik Org PowerShell script

---
+++ PowerShell/SQL-Add-AG-to-Org.ps1
@@ -0,0 +1,14 @@
+.\Setup-Prereq.ps1
+
+$agName = 'am1-sql16ag-1ag'
+$orgName = 'miket-test2'
+
+$agId = (Get-RubrikAvailabilityGroup -name $agName).id
+$roleId = (Get-RubrikOrganization -name $orgName).roleId
+$body = '{"authorizationSpecifications":[{"privilege":"ManageRestoreSource","resources":["$agId"]}],"roleTemplate":"Organization"}'
+
+# Using ExecutionContext to handle expanding the $agId property within the single quote
+$bodyProperty = ConvertFrom-Json ($ExecutionContext.InvokeCommand.ExpandString($body))
+
+# Git-r-done
+Invoke-RubrikRESTCall -api internal -Endpoint "role/$roleId/authorization" -Method POST -Body $bodyProperty
