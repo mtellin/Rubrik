1597d89bba3d0e8522dfe7d0279dab58224acaa7
Mike Tellinghuisen <mtellin@users.noreply.github.com>
2020-08-29 13:28:31 +0000

    added beginning of shell script for getting fileset progress

---
+++ Shell/Get-Fileset-Progress.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# REQUIREMENTS:
+#   - curl is required to interact with the API
+#   - jq is required to parse JSON responses
+
+#Set the following environment variables:
+#$RUBRIK_NODE
+#$RUBRIK_USER
+#$RUBRIK_PASS
+
+FILESET_ID='Fileset:::fddfc2fa-b67a-4f1c-b920-b31c45dc8661'
